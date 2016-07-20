8e1cc6395bdd3d20d372df6268349ac88c29ee4c
Alessandro Argentieri <alexmawashi87@gmail.com>
2016-07-20 11:58:07 +0200

    Update AlbumsAdapter.java

--- app/src/main/java/info/androidhive/cardview/AlbumsAdapter.java
+++ app/src/main/java/info/androidhive/cardview/AlbumsAdapter.java
@@ -9,46 +9,93 @@ import android.widget.TextView;
 
 import java.util.List;
 
-/**
- * Created by Lincoln on 18/05/16.
- */
-public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.MyViewHolder> {
+public class AlbumsAdapter extends ArrayAdapter<Album> {
 
+    private Context mContext;
     private List<Album> albumList;
-
+    /*
     public class MyViewHolder extends RecyclerView.ViewHolder {
-        public TextView title;
-        public ImageView thumbnail;
+        public TextView title, count;
+        public ImageView thumbnail, overflow;
 
         public MyViewHolder(View view) {
             super(view);
             title = (TextView) view.findViewById(R.id.title);
+            count = (TextView) view.findViewById(R.id.count);
             thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+            overflow = (ImageView) view.findViewById(R.id.overflow);
         }
-    }
+    }*/
 
 
-    public AlbumsAdapter(List<Album> albumList) {
+    public AlbumsAdapter(Context mContext, List<Album> albumList) {
+        super(mContext,R.layout.album_card,albumList);
+        this.mContext = mContext;
         this.albumList = albumList;
     }
 
     @Override
-    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View itemView = LayoutInflater.from(parent.getContext())
-                .inflate(R.layout.album_card, parent, false);
+    public View getView(int position, View convertView, ViewGroup parent){
+        //1. Create inflater
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        //2. Get the row View from inflater
+        View rowView = inflater.inflate(R.layout.album_card, parent, false);
+        //3. Get the elements of album_card_layout
+        ImageView thumbImage = (ImageView) rowView.findViewById(R.id.thumbnail);
+        TextView titleText = (TextView) rowView.findViewById(R.id.title);
+        TextView countText = (TextView) rowView.findViewById(R.id.count);
+        final ImageView dotsImage = (ImageView) rowView.findViewById(R.id.overflow);
 
-        return new MyViewHolder(itemView);
-    }
-
-    @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
         Album album = albumList.get(position);
-        holder.title.setText(album.getName());
-        holder.thumbnail.setImageResource(album.getThumbnail());
+        titleText.setText(album.getName());
+        countText.setText(album.getNumOfSongs() + " songs");
+        Glide.with(mContext).load(album.getLink()).into(thumbImage);
+        dotsImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(dotsImage);
+            }
+        });
+
+        return rowView;
     }
 
-    @Override
-    public int getItemCount() {
-        return albumList.size();
+
+    /**
+     * Showing popup menu when tapping on 3 dots
+     */
+    private void showPopupMenu(View view) {
+        // inflate menu
+        PopupMenu popup = new PopupMenu(mContext, view);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.menu_album, popup.getMenu());
+        popup.setOnMenuItemClickListener(new MyMenuItemClickListener());
+        popup.show();
     }
+
+
+    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+
+        public MyMenuItemClickListener() {
+        }
+
+        @Override
+        public boolean onMenuItemClick(MenuItem menuItem) {
+            switch (menuItem.getItemId()) {
+                case R.id.action_add_favourite:
+                    Toast.makeText(mContext, "Add to favourite", Toast.LENGTH_SHORT).show();
+                    return true;
+                case R.id.action_play_next:
+                    Toast.makeText(mContext, "Play next", Toast.LENGTH_SHORT).show();
+                    return true;
+                default:
+            }
+            return false;
+        }
+    }
+
+    //@Override
+    //public int getItemCount() {
+  //      return albumList.size();
+  //  }
 }
